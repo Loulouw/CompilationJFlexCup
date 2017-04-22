f25c6e57b8013311135ace86c1e8dcd4dc13fd5d
Rudy <rudy.grandjean@gmail.com>
2017-04-22 18:10:48 +0200

    test

--- src/fr/ul/miage/exemple/assembleur/Assembleur.java
+++ src/fr/ul/miage/exemple/assembleur/Assembleur.java
@@ -1,6 +1,8 @@
 package fr.ul.miage.exemple.assembleur;
 
 import java.util.*;
+
+import fr.ul.miage.exemple.arbre.Affectation;
 import fr.ul.miage.exemple.arbre.Noeud;
 import fr.ul.miage.exemple.arbre.Programme;
 import fr.ul.miage.exemple.symbol.Symbol;
@@ -11,11 +13,14 @@ import fr.ul.miage.exemple.symbol.SymbolVarGlob;
 public class Assembleur {
 
 	private Programme programme;
+	private ArrayList<Noeud> fonctions;
+	private int posFonc = 0;
 	private ArrayList<Symbol> symbols;
 	private String fichierAssembleur;
 	
 	public Assembleur(Programme programme, ArrayList<Symbol> symbols){
 		this.programme = programme;
+		this.fonctions = programme.getFonctions();
 		this.symbols = symbols;
 		fichierAssembleur = ".include beta.uasm\n" +
 		"CMOVE(pile, SP)\n" +
@@ -30,15 +35,42 @@ public class Assembleur {
 			if(symbol instanceof SymbolVarGlob){
 				fichierAssembleur += symbol.getNom() + ": LONG(0)\n"; 
 			}else if (symbol instanceof SymbolFunc) {
-				genererFonction(symbol.getNom());
+				fichierAssembleur += genererFonction(symbol.getNom());
+				posFonc ++;
 			}
 		}
 		
 		System.out.println(fichierAssembleur);
 	}
 
-	private void genererFonction(String nom) {
-		fichierAssembleur += nom + ":" + programme.toString();
+	private String genererFonction(String nom) {
+		String res = nom + ": PUSH(LP)\n" +
+		"PUSH(BP)\n" + 
+		"MOVE(SP,BP)\n";
+		
+		
+		for (Noeud noeud : fonctions.get(posFonc). ) {
+			res += genererInstructions(noeud);
+		}
+		
+		res += "POP(BP)\n" + 
+		"POP(LP)\n" + 
+		"RCN()\n";
+		
+		return res;
+	}
+
+	private String genererInstructions(Noeud noeud) {
+		String res = "";
+		if(noeud instanceof Affectation){
+			res += genererAffectation(noeud.get());
+		}
+		return res;
+	}
+
+	private String genererAffectation(Noeud noeud) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 	
 	
