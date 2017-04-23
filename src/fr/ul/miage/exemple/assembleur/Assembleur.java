bc4fb5274c5befdcb31aa7c550bd306e49bba1a4
Rudy <rudy.grandjean@gmail.com>
2017-04-24 00:31:49 +0200

    Addition

--- src/fr/ul/miage/exemple/assembleur/Assembleur.java
+++ src/fr/ul/miage/exemple/assembleur/Assembleur.java
@@ -12,6 +12,7 @@ public class Assembleur {
 
 	private Programme programme;
 	private ArrayList<Fonction> fonctions;
+	private ArrayList<String> symbolsVarGlobs;
 	private int posFonc = 0;
 	private ArrayList<Symbol> symbols;
 	private String fichierAssembleur;
@@ -20,40 +21,50 @@ public class Assembleur {
 		this.programme = programme;
 		this.fonctions = programme.getFonctions();
 		this.symbols = symbols;
+		this.symbolsVarGlobs = new ArrayList<String>();
 		fichierAssembleur = ".include beta.uasm\n" +
-		"CMOVE(pile, SP)\n" +
-		"CALL(start)\n" +
-		"HALT()\n\n";
+		"BR(start)\n\n";
 	}
 	
 	public void generer(){
 		System.out.println("ASSEMBLEUR");
 		
+		int i = 0;
 		for (Symbol symbol : symbols) {
 			if(symbol instanceof SymbolVarGlob){
-				fichierAssembleur += symbol.getNom() + ": LONG(0)\n"; 
+				SymbolVarGlob sym = (SymbolVarGlob)symbol;
+				symbolsVarGlobs.add("LD(" + sym.getNom() + ",R" + i +")\n");
+				fichierAssembleur += sym.getNom() + ": LONG(" + sym.getVal() + ")\n"; 
 			}else if (symbol instanceof SymbolFunc) {
-				fichierAssembleur += genererFonction(symbol.getNom());
+				fichierAssembleur += "\n" + genererFonction(symbol.getNom());
 				posFonc ++;
 			}
+			i++;
 		}
 		
 		System.out.println(fichierAssembleur);
 	}
 
 	private String genererFonction(String nom) {
-		String res = nom + ": PUSH(LP)\n" +
+		String res = nom + ":\n";
+		if(nom.equals("start")){
+			for (String str : symbolsVarGlobs) {
+				res += str;
+			}
+		}
+		
+		/*res += "PUSH(LP)\n" +
 		"PUSH(BP)\n" + 
-		"MOVE(SP,BP)\n";
+		"MOVE(SP,BP)\n"; // + 
+		"ALLOCATE(" + symbols.get(posFonc).getNbBLoc + ")";*/
 	
 		
 		for (Noeud noeud : fonctions.get(posFonc).getNoeuds() ) {
 			res += genererInstructions(noeud);
 		}
 		
-		res += "POP(BP)\n" + 
-		"POP(LP)\n" + 
-		"RCN()\n";
+		/*res += "POP(BP)\n" + 
+		"POP(LP)\n";*/
 		
 		return res;
 	}
@@ -69,37 +80,45 @@ public class Assembleur {
 
 	private String genererAffectation(Noeud noeudGauche,Noeud noeudDroit) {
 		Variable partieGauche = (Variable)noeudGauche;	
-		String res = getCalcul(noeudDroit) + 
-		"POP(R0)\n" + 
-		"STORE(R0," + symbols.get(partieGauche.getPlace()).getNom() +")\n";	
+		String res = getCalcul(noeudDroit, "R"+partieGauche.getPlace(), symbols.get(partieGauche.getPlace()).getNom());// + 
+		//"POP(R0)\n" + 
+		//"ST(R0," + symbols.get(partieGauche.getPlace()).getNom() +")\n";	
 		
 		return res;
 	}
 	
-	private String getCalcul(Noeud n){
+	private String getCalcul(Noeud n, String R, String var){
 		String res = "";
 		if(n instanceof Variable){
 			Variable v = (Variable)n;	
-			res += "LD(" + symbols.get(v.getPlace()).getNom() + ",RO)\n" +
-			"PUSH(R0)\n";
+			res += "LD(" + symbols.get(v.getPlace()).getNom() + "," + R + ")\n" +
+			"PUSH("+ R +")\n";
 		}else if(n instanceof Constante){
 			Constante c = (Constante)n;
-			res += "MOVE(" + c.getValeur() + ",R0)\n" + 
-			"PUSH(R0)\n";				
+			res += "CMOVE(" + c.getValeur() + "," + R + ")\n" + 
+			"PUSH("+ R +")\n";				
 		}else if(n instanceof AppelFonction){
 			AppelFonction f = (AppelFonction)n;
 			
 		}else if(n instanceof Calcul){
 			Noeud g = ((Calcul)n).getPartieGauche();
-			String partieGauche = getCalcul(g);
+			String partieGauche = getCalcul(g, "R0", var);
 			
 			Noeud d = ((Calcul)n).getPartieDroite();
-			String partieDroite = getCalcul(d);
+			String partieDroite = getCalcul(d, "R1", var);
 			
 			if(n instanceof Addition){
-				
+				res += partieGauche +"\n"+
+				partieDroite +"\n"+
+				"LD(x,R0) \n"+
+				"ADD(R0,R1,R2)\n" +
+				"ST(R2," + var + ")\n";
 			}else if(n instanceof Soustraction){
-				
+				res += partieGauche +"\n"+
+				partieDroite +"\n"+
+				"LD(x,R0) \n"+
+				"ADD(R0,R1,R2)\n" +
+				"ST(R2," + var + ")\n";
 			}else if(n instanceof Multiplication){
 				
 			}else if(n instanceof Division){
